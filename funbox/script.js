2baebc7ec7c48dc5f9273a14d95d166d08f498bf
baurinvladislav <baurin0@gmail.com>
2018-10-30 15:42:08 +0300

    Update Site

--- funbox/script.js
+++ funbox/script.js
@@ -12,23 +12,23 @@ var inscription3 = document.getElementsByClassName('inscription3');
 
 //Для первого элемента
 function selected1(obj) {
-    if(onElement1==false){
+    if(onElement1==false && disabledElement1==false){
             obj.style.background = 'url(images/backCatSelected.png)';
             inscription1[0].innerHTML='Печень утки разварная с артишоками.';
             onElement1=true;
         }
-    else if (onElement1==true){
+    else if (onElement1==true && disabledElement1==false){
             obj.style.background = 'url(images/backCatDefaultHover.png)';
             inscription1[0].innerHTML= "Чего сидишь? Порадуй котэ, <u>купи</u>.";
             onElement1=false;
         }
 }
 function selectedHoverOff1(obj){
-    if(onElement1==true){
+    if(onElement1==true && disabledElement1==false){
         dynamicText1[0].style.color = "#d91667";
         dynamicText1[0].innerHTML='Котэ не одобряет?';
     }
-    else{ 
+    else if(disabledElement1==false){ 
         dynamicText1[0].style.color = '#666666';
         obj.style.background = 'url(images/backCatDefault.png)';
     }
@@ -36,24 +36,24 @@ function selectedHoverOff1(obj){
 }
 
 function selectedHoverOn1(obj){
-    if(onElement1==true){
+    if(onElement1==true && disabledElement1==false){
         dynamicText1[0].style.color = "#666666";
         dynamicText1[0].innerHTML='Сказочное заморское яство';
     }
-    else if(onElement1==false){
+    else if(onElement1==false && disabledElement1==false){
     obj.style.background = 'url(images/backCatDefaultHover.png)';
     }
 }
 
 //Для второго элемента
 function selected2(obj) { 
-    if(onElement2==false)
+    if(onElement2==false && disabledElement2==false)
         {
             obj.style.background = 'url(images/backCatSelected.png)';
             inscription2[0].innerHTML='Головы щучьи с чесноком да свежайшая сёмгушка.';
             onElement2 = true;
         }
-        else if (onElement2==true)
+        else if (onElement2==true && disabledElement2==false)
         {
             obj.style.background = 'url(images/backCatDefaultHover.png)';
             inscription2[0].innerHTML= "Чего сидишь? Порадуй котэ, <u>купи</u>.";
@@ -61,21 +61,21 @@ function selected2(obj) {
         }
 }
 function selectedHoverOff2(obj){
-    if(onElement2==true){
+    if(onElement2==true && disabledElement2==false){
         dynamicText2[0].style.color = "#d91667";
         dynamicText2[0].innerHTML='Котэ не одобряет?';
     }
-    else{ 
+    else if(disabledElement2==false){ 
         dynamicText2[0].style.color = '#666666';
         obj.style.background = 'url(images/backCatDefault.png)';
     }
 }
 function selectedHoverOn2(obj){
-    if(onElement2==true){
+    if(onElement2==true && disabledElement2==false){
         dynamicText2[0].style.color = "#666666";
         dynamicText2[0].innerHTML='Сказочное заморское яство';
     }
-    else if(onElement2==false){
+    else if(onElement2==false && disabledElement2==false){
         obj.style.background = 'url(images/backCatDefaultHover.png)';
         }
 }
@@ -117,7 +117,7 @@ function selectedHoverOn3(obj){
         }
 }
  
-
+//Отвключение элементов (снятие позиции с продажи)
 var disabledElement1 = false;// если true, значит товара нету.
 var disabledElement2 = false;// если true, значит товара нету.
 var disabledElement3 = true;// если true, значит товара нету.
@@ -127,6 +127,20 @@ var headerElement3 = document.getElementsByClassName('headerElement3');
 
 function disabledElements(){
     console.log(document.getElementById('element3').txt);
+    if(disabledElement1==true){
+        document.getElementById('element1').style.background = 'url(images/backCatDisabled.png)';
+        document.getElementsByClassName('headerElement1')[0].style.color = "#b3b3b3";
+        document.getElementsByClassName('headerElement1')[1].style.color = "#b3b3b3";
+        inscription3[0].innerHTML='Печалька, с фуа-гра закончился.';
+        document.getElementsByClassName('inscription1')[0].style.color = "#ffff66";
+    }
+    if(disabledElement2==true){
+        document.getElementById('element2').style.background = 'url(images/backCatDisabled.png)';
+        document.getElementsByClassName('headerElement2')[0].style.color = "#b3b3b3";
+        document.getElementsByClassName('headerElement2')[1].style.color = "#b3b3b3";
+        inscription3[0].innerHTML='Печалька, с рыбой закончился.';
+        document.getElementsByClassName('inscription2')[0].style.color = "#ffff66";
+    }
     if(disabledElement3==true){
         document.getElementById('element3').style.background = 'url(images/backCatDisabled.png)';
         document.getElementsByClassName('headerElement3')[0].style.color = "#b3b3b3";
