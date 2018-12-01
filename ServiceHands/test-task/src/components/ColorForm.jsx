a1f80a12a32cd085a0bf873c02994d093aa7cd7a
baurinvladislav <baurin0@gmail.com>
2018-12-01 10:00:43 +0300

    update

--- ServiceHands/test-task/src/components/ColorForm.jsx
+++ ServiceHands/test-task/src/components/ColorForm.jsx
@@ -18,9 +18,7 @@ class ColorForm extends React.Component{
 
     ouputColor(event)
     {   event.preventDefault();
-        console.log('Ответ от формы:' + this.state.colorOne + ", " + this.state.colorTwo);
         if((this.state.colorOne==='')&&(this.state.colorOne==='')){
-            console.log('не корректно введенные данные'); 
             this.setState({
                 error: "Не корректно введенные данные. Пожалуйста, проверьте поля для заполнения"
             })
@@ -42,8 +40,6 @@ class ColorForm extends React.Component{
             (event.target.value.length === 7)&&
             (event.target.value.charAt(0)==='#')
             ){
-            console.log("Изменислся первый цвет: " + event.target.value);
-            console.log("Длина 1-го цвета: " + event.target.value.length);
             this.setState({
                 colorOne: event.target.value
             });
@@ -56,14 +52,10 @@ class ColorForm extends React.Component{
             (event.target.value.length === 7)&&
             (event.target.value.charAt(0)==='#')
             ){
-                console.log("Изменислся второй цвет: " + event.target.value)
-                console.log("Изменислся второй цвет: " + event.target.value);
-                console.log("Длина 2-го цвета: " + event.target.value.length);
                 this.setState({
                 colorTwo: event.target.value,
             });
         }
-       else console.log("Введено не коррекное значение");
     }
 
     render(){
