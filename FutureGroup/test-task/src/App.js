e0ed08d0e73aa62a784c355801360c0816328ff7
baurinvladislav <baurin0@gmail.com>
2018-12-05 23:29:40 +0300

    update

---
+++ FutureGroup/test-task/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Tables from './components/Table';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Titel</h1>
+        <Tables />
+      </div>
+    );
+  }
+}
+export default App;
